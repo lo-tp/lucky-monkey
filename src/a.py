04bbcdb6592aa18c0b78893d39279de8453dff68
lo-tp <regesteraccount@hotmail.com>
2020-09-25 12:22:40 +0800

    write the request model

--- src/a.py
+++ src/a.py
@@ -3,21 +3,12 @@ from sys import argv
 from json import dumps
 from requests.exceptions import HTTPError
 
-data = {
-    "api_name": "stock_basic",
-    "token": argv[1],
-    "params": {
-        "list_stauts": "L"
-    },
-    "fields": "ts_code,name,area,industry,list_date"
-}
 
-
-def sendRequest():
+def sendRequest(arg):
     try:
         response = requests.post(
             "http://api.waditu.com",
-            data=dumps(data))
+            data=dumps(arg))
         # If the response was successful, no Exception will be raised
         response.raise_for_status()
     except HTTPError as http_err:
@@ -25,8 +16,18 @@ def sendRequest():
     except Exception as err:
         print(f'Other error occurred: {err}')  # Python 3.6
     else:
-        print(response.json())
-        print('Success!')
+        return response.json()
 
 
-sendRequest()
+if __name__ == "__main__":
+    data = {
+        "api_name": "daily",
+        "token": argv[1],
+        "params": {
+            "ts_code": "1A0001",
+            "start_date": 20181010,
+            "end_date": 20181020
+        },
+        "fields": "ts_code, trade_date, open"
+    }
+    print(sendRequest(data))
